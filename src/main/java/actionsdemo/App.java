6b52205a70809b1a1dc478bdf1827852bc8c6f80
hathwarkoushik <hathwarkoushik@gmail.com>
2022-10-27 10:44:16 +0100

    adding the project

---
+++ src/main/java/actionsdemo/App.java
@@ -0,0 +1,13 @@
+package actionsdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
